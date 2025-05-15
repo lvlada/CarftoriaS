341c8816390ad5249660e794bcdd4c577778ccd9
lvlada <vladimirlukic197@gmail.com>
2025-05-15 19:02:10 +0200

    feat: finished

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,18 @@
-import pera from './Banner.module.scss';
+import { BannerItem } from './BannerItem';
+import style from './Banner.module.scss';
+import image from '@/assets/BannerPlaceHolder.png';
+
 const Banner = () => {
-  return <div className={pera.banner}>banner</div>;
+  return (
+    <section className={style.banner}>
+      <BannerItem image={image} title="Naziv firme" />
+      <BannerItem image={image} title="Naziv firme" />
+      <BannerItem image={image} title="Naziv firme" />
+      <BannerItem image={image} title="Naziv firme" />
+      <BannerItem image={image} title="Naziv firme" />
+      <BannerItem image={image} title="Naziv firme" />
+    </section>
+  );
 };
 
 export { Banner };
