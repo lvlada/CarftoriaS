17813395892139f10eebe1ed67d24ec6a1394134
MilosIl <milos.ilic20@hotmail.com>
2025-06-02 12:04:52 +0200

    feat: button styles and update input component

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,10 @@
 import { LoginFormContainer } from './components';
-
+import style from './LoginPage.module.scss';
 const LoginPage = () => {
   return (
-    <>
+    <div className={style.loginPage}>
       <LoginFormContainer />
-    </>
+    </div>
   );
 };
 
--- src/pages/LoginPage/components/LoginForm.jsx
+++ src/pages/LoginPage/components/LoginForm.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router';
 import style from './LoginForm.module.scss';
 const LoginForm = () => {
   return (
-    <div className={(style.loginFormtype = 'submit')}>
-      <h1>Dobrodošli nazad!</h1>
+    <div className={style.loginForm__container}>
+      <h1 className={style.loginForm__title}>Dobrodošli nazad!</h1>
       <p>Ulogujte se na svoj profil</p>
-      <form>
-        <Input placeholder={'Email adreasa'} type="email" />
-        <Input placeholder={'Šifra'} type="password" />
+      <form className={style.loginForm__inputs}>
+        <Input placeholder={'Email adreasa'} type="email" className={style.input} />
+        <Input placeholder={'Šifra'} type="password" className={style.input} />
       </form>
       <Link>Zaboraviljena šifra?</Link>
       <Button type="submit">Uloguj se</Button>
       <p>ili nastavi sa</p>
-      <Button variant="secondary" icon={<IconGoogle />}>
+      <Button variant="outline" icon={<IconGoogle />} className={style.loginForm__btn}>
         Google
       </Button>
       <p>
