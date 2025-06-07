c0a1281c95df4e50d42969103872d3067e22e6cd
lvlada <vladimirlukic197@gmail.com>
2025-06-07 20:48:40 +0200

    feat: different routing

--- src/providers/Provider.jsx
+++ src/providers/Provider.jsx
@@ -1,6 +1,12 @@
 import { BrowserProvider } from './BrowserProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Provider = ({ children }) => {
-  return <BrowserProvider>{children}</BrowserProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <BrowserProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </BrowserProvider>
+  );
 };
 export { Provider };
