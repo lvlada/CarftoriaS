5ff9d542f7f5282c0304792ab123a3178b277385
lvlada <vladimirlukic197@gmail.com>
2025-06-09 13:27:00 +0200

    feat: new features and fixin bugs

--- src/components/ProfilCard/ProfilCardsContainer.jsx
+++ src/components/ProfilCard/ProfilCardsContainer.jsx
@@ -11,6 +11,8 @@ import { useScroll } from '@/context/ScrollContext';
 const ProfilCardsContainer = () => {
   const [newList, setNewList] = useState([]);
   const [ratingAsc, setRatingAsc] = useState(true);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
   const { profileCardsRef } = useScroll();
 
   const { data, isLoading } = useQuery({
@@ -26,15 +28,32 @@ const ProfilCardsContainer = () => {
     }
   }, [data]);
 
+  // sortiranje
   const fileterByRating = () => {
     setNewList(sortUsersByRating(newList, ratingAsc));
     setRatingAsc(!ratingAsc);
+    setPage(1);
   };
 
   const filterByComments = () => {
     setNewList(sortUsersByComments(newList, ratingAsc));
     setRatingAsc(!ratingAsc);
+    setPage(1);
   };
+
+  // paginacija
+  const totalPages = Math.ceil(newList.length / perPage);
+  const start = (page - 1) * perPage;
+  const end = start + perPage;
+  const paginatedUsers = newList.slice(start, end);
+
+  const handlePerPage = (num) => {
+    setPerPage(num);
+    setPage(1);
+  };
+
+  const handlePrev = () => setPage((p) => (p > 1 ? p - 1 : 1));
+  const handleNext = () => setPage((p) => (p < totalPages ? p + 1 : totalPages));
 
   if (isLoading) return <div>Loading....</div>;
 
@@ -62,18 +81,47 @@ const ProfilCardsContainer = () => {
           />
         </span>
       </p>
-      <ProfileCards users={newList} />
+      <ProfileCards users={paginatedUsers} />
       <div className={style.profilCardsContainer__pagination}>
         <p>
-          <span className={style.paginationArrow}>
+          <span
+            className={style.paginationArrow}
+            onClick={handlePrev}
+            style={{ cursor: 'pointer' }}
+          >
             <IconArrowLeftSolid />
           </span>
-          Strana 1/3
-          <span className={style.paginationArrow}>
+          Strana {page}/{totalPages}
+          <span
+            className={style.paginationArrow}
+            onClick={handleNext}
+            style={{ cursor: 'pointer' }}
+          >
             <IconArrowRightSolid />
           </span>
         </p>
-        <p>Prikaži po strani: 10 30 50 sve</p>
+        <p>
+          Prikaži po strani:
+          <span style={{ cursor: 'pointer', marginLeft: 8 }} onClick={() => handlePerPage(10)}>
+            {' '}
+            10{' '}
+          </span>
+          <span style={{ cursor: 'pointer', marginLeft: 8 }} onClick={() => handlePerPage(30)}>
+            {' '}
+            30{' '}
+          </span>
+          <span style={{ cursor: 'pointer', marginLeft: 8 }} onClick={() => handlePerPage(50)}>
+            {' '}
+            50{' '}
+          </span>
+          <span
+            style={{ cursor: 'pointer', marginLeft: 8 }}
+            onClick={() => handlePerPage(newList.length)}
+          >
+            {' '}
+            sve{' '}
+          </span>
+        </p>
       </div>
     </section>
   );
