d7c64e8e6bec94f6aba3e20239fe6073c9608a24
lvlada <vladimirlukic197@gmail.com>
2025-06-10 14:48:29 +0200

    fix: login mix

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const login = async (email, password) => {
   try {
-    const response = await axios.post(`http://63.178.38.11:8080/CraftoriaS/api/login`, null, {
+    const response = await axios.post(`/api/login`, null, {
       params: { email, password }
     });
     return response.data;
